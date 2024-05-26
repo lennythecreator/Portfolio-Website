cf8b3939a94d21d5bdabc2019cb7c490b5d3f973
Lenny Uwaeme <48912140+lennythecreator@users.noreply.github.com>
2024-05-26 09:48:02 -0400

    Created the projects page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import '../styles/components/header.css' 
+import '../styles/components/about.css'
+import '../styles/components/cards.css'
 import '../styles/components/hero.css'
 import '../styles/style.css'
 import '../styles/util.css'
+
+
+const modal = document.querySelector('dialog')
+const tag = document.querySelector('tag')
+
+if (tag&&modal){
+    tag.addEventListener("click",()=>{
+        modal.showModal()
+    });
+}
